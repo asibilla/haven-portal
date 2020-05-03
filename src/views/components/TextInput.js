120082387e5dbcbd728f94131e86f651f78c8134
asibilla <mr.andy.sibilla@gmail.com>
2020-05-02 20:53:31 -0700

    add inputs for username password

---
+++ src/views/components/TextInput.js
@@ -0,0 +1,26 @@
+import { func, string } from 'prop-types';
+import React from 'react';
+
+
+const TextInput = ({ onChange, placeholder, type, value }) => {
+    return (
+        <label>
+            <input onChange={onChange} type={type} placeholder={placeholder} value={value} />
+        </label>
+    );
+};
+
+TextInput.defaultProps = {
+    placeholder: '',
+    type: 'text',
+    value: '',
+};
+
+TextInput.propTypes = {
+    onChange: func.isRequired,
+    placeholder: string,
+    type: string,
+    value: string,
+};
+
+export default TextInput;
--- src/views/routes/UserSignIn.js
+++ src/views/routes/UserSignIn.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { TextInput } from '../components';
 
 const UserSignin = () => {
-  return <h1>User Sign In Page</h1>;
+  const [ userName, setUserName ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const updateUserName = (e) => setUserName(e.target.value);
+  const updatePassword = (e) => setPassword(e.target.value);
+
+  return (
+    <div>
+      <h1>
+        User Sign In Page
+      </h1>
+      <TextInput 
+        onChange={updateUserName}
+        placeholder="username"
+        value={userName}
+      />
+      <TextInput 
+        onChange={updatePassword}
+        placeholder="password"
+        type="password"
+        value={password}
+      />
+    </div>
+  );
 };
 
 export default UserSignin;
