cf0dbbfdcce42310852faa4e6132996883e0a66c
asibilla <mr.andy.sibilla@gmail.com>
2020-05-09 20:32:15 -0700

    style and add manage options page

--- src/views/components/Button.js
+++ src/views/components/Button.js
@@ -1,21 +1,26 @@
 import { noop } from 'lodash';
 import { func, string } from 'prop-types';
 import React from 'react';
+import { cx } from 'react-emotion';
 
-const Button = ({ onClick, text, type }) => {
+import { styles } from '../../constants';
+
+const Button = ({ className, onClick, text, type }) => {
   return (
-    <button onClick={onClick} type={type}>
+    <button className={cx(styles.button, className)} onClick={onClick} type={type}>
       {text}
     </button>
   );
 };
 
 Button.defaultProps = {
+  className: '',
   onClick: noop,
   type: 'button',
 };
 
 Button.propTypes = {
+  className: string,
   onClick: func,
   text: string.isRequired,
   type: string,
--- src/views/components/TextInput.js
+++ src/views/components/TextInput.js
@@ -1,11 +1,16 @@
 import { func, string } from 'prop-types';
 import React from 'react';
+import { cx } from 'react-emotion';
 
-const TextInput = ({ onChange, placeholder, type, value }) => {
+import { styles } from '../../constants';
+
+const TextInput = ({ className, labelText, onChange, placeholder, type, value }) => {
   return (
     <label htmlFor={placeholder}>
+      {labelText && `${labelText}`}
       <input
         id={placeholder}
+        className={cx(styles.textInput, className)}
         onChange={onChange}
         type={type}
         placeholder={placeholder}
@@ -16,12 +21,16 @@ const TextInput = ({ onChange, placeholder, type, value }) => {
 };
 
 TextInput.defaultProps = {
+  className: '',
+  labelText: '',
   placeholder: '',
   type: 'text',
   value: '',
 };
 
 TextInput.propTypes = {
+  className: string,
+  labelText: string,
   onChange: func.isRequired,
   placeholder: string,
   type: string,
