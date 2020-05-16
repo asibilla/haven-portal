8983ecb35529af337641ea42b2a5622bc1dbe643
asibilla <mr.andy.sibilla@gmail.com>
2020-05-16 15:53:46 -0700

    finish form

--- src/constants/styles/manageOptions.js
+++ src/constants/styles/manageOptions.js
@@ -1,16 +1,23 @@
 import { css, cx } from 'react-emotion';
 import { desktop, flexRow } from './global';
 
+export const buttonContainer = css`
+  padding-top: 15px;
+  text-align: right;
+`;
+
 export const formContainer = cx(
+  'manage-options-form',
   css`
     ${desktop} {
       ${flexRow}
       align-items: flex-start;
+      justify-content: center;
     }
   `
 );
 
 export const formSection = css`
   padding: 0 15px;
-  width: 50%;
+  width: calc(50% - 35px);
 `;
