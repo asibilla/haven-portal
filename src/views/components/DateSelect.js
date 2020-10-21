b6adb14f81262773af7e3514b9f84e8858be04bf
asibilla <mr.andy.sibilla@gmail.com>
2020-10-21 09:59:40 -0700

    re-insert datepickers into scheduler

--- src/views/components/DateSelect.js
+++ src/views/components/DateSelect.js
@@ -1,33 +1,57 @@
-import { func, string } from 'prop-types';
+import { bool, func, string } from 'prop-types';
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 import { styles } from '../../constants';
+import { toDate } from '../../helpers/dateFormatter';
 
-const DateSelect = ({ error, instructions, labelText, onChange, placeholder, startDate }) => {
+const DateSelect = ({
+  error,
+  disabled,
+  instructions,
+  labelText,
+  maxDate,
+  minDate,
+  onChange,
+  placeholder,
+  startDate,
+}) => {
   return (
     <label htmlFor={placeholder}>
       {labelText && <div className={styles.label}>{labelText}</div>}
       {instructions && <div className={styles.fieldInstructions}>{instructions}</div>}
-      <DatePicker onChange={onChange} selected={new Date(startDate)} />
+      <DatePicker
+        disabled={disabled}
+        maxDate={maxDate ? toDate(maxDate) : ''}
+        minDate={minDate ? toDate(minDate) : ''}
+        onChange={onChange}
+        placeholderText="Select a Date"
+        selected={startDate ? toDate(startDate) : ''}
+      />
       <div className={styles.inputError}>{error && `${error}`}</div>
     </label>
   );
 };
 
 DateSelect.defaultProps = {
+  disabled: false,
   error: '',
   instructions: '',
   labelText: '',
+  maxDate: '',
+  minDate: new Date().toString(),
   placeholder: '',
   startDate: new Date().toString(),
 };
 
 DateSelect.propTypes = {
+  disabled: bool,
   error: string,
   instructions: string,
   labelText: string,
+  maxDate: string,
+  minDate: string,
   onChange: func.isRequired,
   placeholder: string,
   startDate: string,
--- webpack.config.js
+++ webpack.config.js
@@ -37,18 +37,6 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
-            },
-          },
-        ],
-      },
     ],
   },
   plugins: [
